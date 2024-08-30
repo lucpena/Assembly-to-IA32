83ce5f9e9da3f0236fe9b6e7b3c5bb5572fe3430
Lucas Pena <lucaspena4@gmail.com>
2024-08-30 15:41:26 -0300

    commit

--- main.cpp
+++ main.cpp
@@ -46,49 +46,101 @@ int main( int argc, char *argv[] )
     }
     input.close();
 
+    // Inicializando o programa em IA32
+    translator.initializateCode();
 
     // Traduzindo o Assembly
-    for (int i = 0; i != program_source.size(); ++i)
+    // Primeira passagem para definir os espacos de memoria
+    for (int i = 0; i < program_source.size(); ++i)
     {
-       cout << program_source[i] << " ";
+        // Adquirindo o tamanho da intrucao
+        int memSpcae = getMemorySpace(program_source[i]);
 
-       // Adquirindo o tamanho da intrucao
-       int memSpcae = getMemorySpace(program_source[i]);
-
-       switch (stoi(program_source[i]))
-       {
-            case 1:  // ADD
-                break;
-            case 2:  // SUB
-                break;
-            case 3:  // MUL
-                break;
-            case 4:  // DIV
-                break;
-            case 5:  // JMP
-                break;
-            case 6:  // JMPN
-                break;
-            case 7:  // JMPP
-                break;
-            case 8:  // JMPZ
-                break;
-            case 9:  // COPY
-                break;
-            case 10: // LOAD
-                break;
-            case 11: // STORE
-                break;
-            case 12: // INPUT
-                break;
-            case 13: // OUTPUT
-                break;
-            case 14: // STOP
-                break;
-            default: // Unknown opcode
-                break;
-       }
+        if( !translator.hasStopped() )
+        {
+            switch (stoi(program_source[i]))
+            {
+                case 14: // STOP
+                    translator.setStopped(true);
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            translator.addMemory(i, program_source[i]);
+        }
     }
+
+    // Checa as variaveis e adiciona elas no codigo final
+    translator.createVariables();
+
+    // Segunda passagem para gerar o codigo final
+    translator.setStopped(false);
+    for (int i = 0; i < program_source.size(); ++i)
+    {
+
+        // Adquirindo o tamanho da intrucao
+        int memSpcae = getMemorySpace(program_source[i]);
+
+        if( !translator.hasStopped() )
+        {
+            switch (stoi(program_source[i]))
+            {
+                case 1:  // ADD
+                    i++;
+                    translator.ADD(program_source[i]);
+                    break;
+                case 2:  // SUB
+                    i++;
+                    translator.SUB(program_source[i]);
+                    break;
+                case 3:  // MUL
+                    i++;
+                    translator.MUL(program_source[i]);
+                    break;
+                case 4:  // DIV
+                    i++;
+                    translator.DIV(program_source[i]);
+                    break;
+                case 5:  // JMP
+                    i++;
+                    break;
+                case 6:  // JMPN
+                    i++;
+                    break;
+                case 7:  // JMPP
+                    i++;
+                    break;
+                case 8:  // JMPZ
+                    i++;
+                    break;
+                case 9:  // COPY
+                    translator.COPY(program_source[i + 1],program_source[i + 2]);
+                    i += 2;
+                    break;
+                case 10: // LOAD
+                    i++;
+                    break;
+                case 11: // STORE
+                    i++;
+                    break;
+                case 12: // INPUT
+                    i++;
+                    translator.INPUT(program_source[i]);
+                    break;
+                case 13: // OUTPUT
+                    i++;
+                    break;
+                case 14: // STOP
+                    translator.STOP();
+                    break;
+                default: // Unknown opcode
+                    translator.addWarning("OPCODE " + program_source[i] + " nao identificado lol");
+                    break;
+            }
+        }
+    }
+
 
     // Parte Opcional
     // Adicionar novos OPCODES ( S_INPUT | S_OUTPUT )
@@ -96,18 +148,29 @@ int main( int argc, char *argv[] )
 
     // Mostrando os resultados
     system("clear");
-    cout << endl;
-    cout << endl;
-    cout << " Lucas Pena [13/0056162]\n";
-    cout << " Tradutor de Assembly Inventado para IA-32.\n";
-    cout << "----------------------------------------------\n\n";
-    cout << "Programa inserido: ";
+    cout << "| Lucas Pena [13/0056162]\n"
+    << "| Tradutor de Assembly Inventado para IA-32.\n"
+    << "----------------------------------------------\n\n"
+    << "Programa inserido\n---------------------------\n ";
+
     for (auto i = program_source.begin(); i != program_source.end(); ++i)
     {
         cout << *i << " ";
     }
-    cout << endl;
-    cin.get();
+
+    cout << "\n\nCodigo gerado \n---------------------------\n";
+    cout << translator.getCode(); 
+    cout << "\n" << endl << "\nWarnings\n---------------------------\n";
+    if(translator.getWarnings() == "")
+    {
+        cout << " Sem warnings.";
+    } else {
+        cout << translator.getWarnings();
+    }
+    cout << "\n\nMemoria\n---------------------------\n";
+    translator.showMemory();
+    // << "Aperte enter para sair..." << endl;
+    // cin.get();
 
     return 0;
 }
--- system.hpp
+++ system.hpp
@@ -15,6 +15,7 @@
 
 using std::endl;
 using std::cout;
+using std::cerr;
 using std::cin;
 using std::getline;
 using std::string;
@@ -50,38 +51,252 @@ map<int, string> instructions =
 	{13, "OUTPUT"}, {14, "STOP"}
 };
 
-map<int, string> IA32instructions = 
+class ResultMessage
 {
-	{1, "ADD"}, {2, "SUB"}, {3, "MUL"},
-	{4, "DIV"}, {5, "JMP"}, {6, "JMPN"},
-	{7, "JMPP"}, {8, "JMPZ"}, {9, "COPY"},
-	{10, "LOAD"}, {11, "STORE"}, {12, "INPUT"},
-	{13, "OUTPUT"}, {14, "STOP"}
+private:
+
+    bool m_Success;
+    string m_Message;
+
+public:
+
+    ResultMessage()
+    : m_Success(true), m_Message("") {};
+
+    ~ResultMessage() {};
+
 };
 
-
-// Variaveis do Tradutor
-vector<string>              program_source = {};
-map<uint32_t, string>       program_IA32   = {};
+/********************************
+    Tradutor
+*********************************/
+vector<string>  program_source  = {};
 
 class Translator
 {
 private:
 
-    string m_Accumulator = "";
+    string m_IA32_data;
+    string m_IA32_bss;
+    string m_IA32_text;
+    string m_Accumulator;
+    string m_Warnings;
+
+    uint32_t m_AllocatedMemory;
+
+    map<int, string> m_Memory;
+    map<int, string> m_MemoryIA32;
+
+    bool m_hasStopped;
 
 public:
 
-    Translator() {}
+    Translator():         
+        m_IA32_data(""), 
+        m_IA32_bss(""), 
+        m_IA32_text(""),
+        m_Accumulator(""),
+        m_Warnings(""),
+        m_AllocatedMemory(0),
+        m_Memory({}),
+        m_MemoryIA32({}),
+        m_hasStopped(false) {}
+
     ~Translator() {};
 
-    string getAccumulator() { return m_Accumulator;}
+    inline string getAccumulator() { return m_Accumulator;}
+    inline void setAccumulator( string value ) { m_Accumulator = value; }
+
+    void ADD( string value );
+    void SUB( string value );
+    void MUL( string value );
+    void DIV( string value );
+    void STORE( string value );
+    void INPUT( string value );
+    void COPY( string dst, string src );
+
+    void STOP();
+
+    void addData( string data );
+    void addBSS( string bss );
+    void addText( string text );
+    void addLabel( string label );
+    void addEqu( string data, string value );
+
+    void addMemory( int add, string value );
+    string getMemoryValue( string add );
+    void createVariables();
+
+    void addWarning( string label );
+    void showMemory();
+
+    void initializateCode();
+    string getCode();
+    string getWarnings();
     
-    void setAccumulator( string value ) { m_Accumulator = value; }
+    inline bool hasStopped() { return m_hasStopped; };
+    inline void setStopped( bool value ) { m_hasStopped = value; };
 
+};
+// Definicoes do Tradutor
+inline void Translator::addLabel( string label )
+{
+    m_IA32_text += label + ":\n";
+}
 
-}; 
+inline void Translator::addData( string data )
+{
+    m_IA32_data += "    " + data + " dd 0\n";
+}
 
+inline void Translator::addEqu( string data, string value )
+{
+    m_IA32_data += "    " + data + " equ " + value + "\n";
+}
+
+inline void Translator::addBSS( string bss )
+{
+    m_IA32_bss += "    " + bss + " resd 0\n";
+}
+
+inline void Translator::addText( string text )
+{
+    m_IA32_text += "    " + text + "\n";
+}
+
+inline void Translator::addWarning( string text )
+{
+    m_Warnings += "- " + text + "\n";
+}
+
+inline void Translator::addMemory( int add, string value )
+{
+    m_Memory[add] = value;
+    m_MemoryIA32[add] = "var" + to_string(add);
+}
+
+inline string Translator::getMemoryValue( string add )
+{
+    return m_Memory[stoi(add)];
+}
+
+void Translator::showMemory()
+{
+    for( auto it = m_Memory.begin(); it != m_Memory.end(); ++it)
+    {
+        cout << " Address: " << it->first << " | Value: " << it->second << endl;
+    }
+    cout << endl;
+    for( auto it = m_MemoryIA32.begin(); it != m_MemoryIA32.end(); ++it)
+    {
+        cout << " Address: " << it->first << " | Value: " << it->second << endl;
+    }
+    cout << endl;
+}
+
+// verifica as variaveis e cria a variavel no codigo IA32
+void Translator::createVariables()
+{
+    for( auto it = m_Memory.begin(); it != m_Memory.end(); ++it)
+    {
+        if(it->second =="0")
+        {
+            if(m_MemoryIA32.find(it->first) != m_MemoryIA32.end())
+            {
+                addBSS( m_MemoryIA32[it->first]);
+            }
+        }
+        else{
+            if(m_MemoryIA32.find(it->first) != m_MemoryIA32.end())
+            {
+                addEqu(m_MemoryIA32[it->first], it->second);
+            }
+        }
+    }
+}
+
+void Translator::initializateCode()
+{
+    m_IA32_data = "section .data\n";
+    m_IA32_bss  = "section .bss\n";
+    m_IA32_text = "section .text\n";
+
+    // Inicializa o Acumulador em memória
+    addData("ACC");
+
+    // Inicializa o start
+    addText("global _start\n");
+    addLabel("_start");
+} 
+
+inline string Translator::getCode()
+{
+    return m_IA32_data + "\n" + m_IA32_bss + "\n" + m_IA32_text;
+}
+
+inline string Translator::getWarnings()
+{
+    return m_Warnings;
+}
+
+/********************************
+    OPCODES
+*********************************
+IA32:
+    ADD DST, SRC
+    ------------------------
+    SRC + DST -> DST
+
+Assembly:
+    [valor ja no ACC]
+    ADD  SRC
+    ------------------------
+    SRC + ACC -> ACC
+*/
+
+inline void Translator::ADD( string value)
+{
+    addText("add [ACC], []" + m_MemoryIA32[stoi(value)] + "]");
+}
+inline void Translator::SUB( string value)
+{
+    addText("sub [ACC], [" + m_MemoryIA32[stoi(value)] + "]");
+}
+inline void Translator::MUL( string value)
+{
+    addText("mov eax, [ACC]");
+    addText("mov ebx, [" + m_MemoryIA32[stoi(value)] + "]");
+    addText("imul ebx");
+    addText("mov [ACC], eax");
+}
+inline void Translator::DIV( string value)
+{
+    addText("mov ecx, [ACC]");
+    addText("mov eax, [" + m_MemoryIA32[stoi(value)] + "]");
+    addText("cdq"); // extensao de sinal
+    addText("idiv ebx");
+    addText("mov [ACC], eax");
+}
+inline void Translator::INPUT( string value )
+{
+    addText("mov [ACC], [" + m_MemoryIA32[stoi(value)] + "]");
+}
+inline void Translator::COPY( string dst, string src)
+{
+    addText("mov [" + m_MemoryIA32[stoi(dst)] + "], [" + m_MemoryIA32[stoi(src)] + "]");
+}
+inline void Translator::STORE( string value )
+{
+    addText("mov " + m_MemoryIA32[stoi(value)] + ", [ACC]");
+}
+inline void Translator::STOP()
+{
+    addText("mov eax, 1");
+    addText("xor ebx, ebx");
+    addText("int 0x80");
+
+    m_hasStopped = true;
+}
 
 /********************************
     Funcoes ajudantes
